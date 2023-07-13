14c99d5b00f37395ce1a84cacabbb97cd235ac1c
Zynh0722 <Zynh0722@gmail.com>
2023-07-13 02:49:56 -0700

    convert poles to remnants

--- ruins/smallRuins/solarAccu.lua
+++ ruins/smallRuins/solarAccu.lua
@@ -6,7 +6,7 @@ return
     {"accumulator", {x = 0, y = -3}, {dmg = {dmg = {type = "random", min = 0, max = 300}}, }},
     {"solar-panel", {x = 2.5, y = -2.5}, {dmg = {dmg = {type = "random", min = 0, max = 400}}, }},
     {"accumulator", {x = -3, y = 0}, {dmg = {dmg = {type = "random", min = 0, max = 300}}, }},
-    {"medium-electric-pole", {x = 0.5, y = -0.5}, {dmg = {dmg = {type = "random", min = 0, max = 100}}, }},
+    {"medium-electric-pole-remnants", {x = 0.5, y = -0.5}, {dmg = {dmg = {type = "random", min = 0, max = 100}}, }},
     {"accumulator", {x = 3, y = 0}, {dmg = {dmg = {type = "random", min = 0, max = 300}}, }},
     {"solar-panel", {x = -2.5, y = 2.5}, {dmg = {dmg = {type = "random", min = 0, max = 400}}, }},
     {"small-lamp", {x = -0.5, y = 0.5}, {dmg = {dmg = {type = "random", min = 0, max = 100}}, }},
